14390bfd768a15d0b6f01cd2124472b7bd0b2115
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-04-23 23:49:11 +0300

    improve deploy script

--- .github/workflows/copy_files_to_gcs.sh
+++ .github/workflows/copy_files_to_gcs.sh
@@ -1,5 +1,7 @@
 gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp src/index.html gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/script.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/notifications.js gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/style.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/examples.css gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/notifications.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:image/png" -h "Cache-Control:no-store" cp -r img gs://${GCP_BUCKET}
