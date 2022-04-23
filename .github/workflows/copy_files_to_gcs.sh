6b6a77f8f674a686612d6735ca247dc72e934728
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-04-23 11:47:19 +0300

    update github actions workflow

---
+++ .github/workflows/copy_files_to_gcs.sh
@@ -0,0 +1,5 @@
+gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp src/index.html gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/script.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/style.css gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/examples.css gs://${GCP_BUCKET}
+gsutil -h "Content-Type:image/png" -h "Cache-Control:no-store" cp -r img gs://${GCP_BUCKET}
