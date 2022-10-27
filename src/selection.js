5dc48db36f66aee8f6fe963e8e75997e8c5dd538
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2022-10-27 20:07:44 +0300

    fix selected bytes amount calculation; now it works on firefox too

--- src/selection.js
+++ src/selection.js
@@ -1,21 +1,26 @@
 
 document.onselectionchange = () => {
-  const selection = window.getSelection().toString();
-
-  if (!selection) {
-    document.getElementById('selectedBytesCounter').innerText = 0;
-    return;
-  }
-
   const element = document.activeElement;
 
   let count = 0;
 
   if (element.id === 'inData') {
+    const selection = element.value.substring(element.selectionStart, element.selectionEnd);
+    if (!selection) {
+      document.getElementById('selectedBytesCounter').innerText = 0;
+      return;
+    }
+
     const inType = document.getElementById('inType').selectedOptions[0].value;
     const data = parsers[inType](selection);
     count = data.length;
   } else if (element.id === 'outData') {
+    const selection = element.value.substring(element.selectionStart, element.selectionEnd);
+    if (!selection) {
+      document.getElementById('selectedBytesCounter').innerText = 0;
+      return;
+    }
+
     const outType = document.getElementById('outType').selectedOptions[0].value;
     const data = parsers[outType](selection);
     count = data.length;
