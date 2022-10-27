6dc22d053ce97c695889b870abd66125733ed3f9
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2022-10-27 23:06:33 +0300

    update deplou script

--- .github/workflows/copy_files_to_gcs.sh
+++ .github/workflows/copy_files_to_gcs.sh
@@ -2,7 +2,9 @@ gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp src/index.html
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/script.js gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/notifications.js gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/selection.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp src/results.js gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/style.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/examples.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/notifications.css gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp src/results.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:image/png" -h "Cache-Control:no-store" cp -r img gs://${GCP_BUCKET}
